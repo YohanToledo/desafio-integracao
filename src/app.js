b6c48a8abe1bacbef1a09cbf922d25c213597a46
YohanToledo <yohan.gabriel.bernart@gmail.com>
2022-07-28 23:47:23 -0300

    view adicionado utilizando ejs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 const router = express.Router();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 //Rotas
 const index = require('./routes/index');
 app.use('/', index);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const requests = require('../controllers/requests');
 
+
 const router = express.Router();
 router.get('/', async function (req, res, next) {
-
-    requests.getSpreadsheetValues(req, res);
-    //requests.sendHubspotContact();
-    
+    res.render('index');
 });
+
+router.post('/', function(req, res, next){
+    requests.getSpreadsheetValues(req, res);
+})
+
 module.exports = router;
